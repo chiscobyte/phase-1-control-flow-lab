ab2132f39b72180f4eac383a9bcc4f8109578c67
Enock Chisiwa <enock.chisiwa@student.moringaschool.com>
2025-04-03 16:05:45 +0300

    initiate

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ function scuberGreetingForFeet(someNumber) {
   if (someNumber <= 400){
           return "This one is on me!";
   }
+
+  else if (someNumber > 400 && someNumber < 2000){
+    return "That will be twenty bucks.";
+  }
+
   else if (someNumber > 2000 && someNumber < 2500){
       return "I will gladly take your thirty bucks.";
   }
@@ -20,8 +25,8 @@ function scuberGreetingForFeet(someNumber) {
      
 }
 
-function switchOnCharmFromTip(trip) {
-  switch (trip){
+function switchOnCharmFromTip(tip) {
+  switch (tip){
     case 'generous':
       return 'thank you so much'
   case 'not as generous':
